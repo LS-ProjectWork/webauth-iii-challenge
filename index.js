3e44bdc544936677677d3228d26db20efc77353d
Darien Payton <darien.payton71@gmail.com>
2019-05-08 15:55:27 -0400

    created register operation

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const knex = require('knex');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
 
@@ -12,4 +13,16 @@ server.use(express.json());
 
 server.listen(7200, () => {
     console.log('Server is listening on port 7200');
+})
+
+server.post('/api/register', (req, res) => {
+    const user = req.body
+
+    user.password = bcrypt.hashSync(user.password, 10)
+
+    db('users')
+    .insert(user)
+    .then(user => {
+        res.status(201).json(user)
+    })
 })
