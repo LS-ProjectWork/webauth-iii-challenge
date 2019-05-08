fdff0b98a5b5776f117689e1bce0965038594317
Darien Payton <darien.payton71@gmail.com>
2019-05-08 16:51:02 -0400

    authenticated login and sent jwt

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const knex = require('knex');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const knexConfig = require('./knexfile');
 const db = knex(knexConfig.development);
@@ -24,5 +25,35 @@ server.post('/api/register', (req, res) => {
     .insert(user)
     .then(user => {
         res.status(201).json(user)
+    })
+})
+
+const secret = "we know far less about ourselves than we do others"
+function generateToken(user){
+    const payload = {
+        subject: user.id,
+        username: user.username
+    };
+    const options = {
+        expiresIn: '1d'
+    };
+    
+    return jwt.sign(payload, secret, options);
+}
+
+server.post('/api/login', (req, res) => {
+    const {username, password} = req.body
+
+    db('users')
+    .where({username})
+    .first()
+    .then(user => {
+        if(user && bcrypt.compareSync(password, user.password)) {
+            const token = generateToken(user);
+
+            res.status(201).json({token})
+        } else {
+            res.status(401).send('Invalid credentials')
+        }
     })
 })
