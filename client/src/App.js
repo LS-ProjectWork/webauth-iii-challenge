54fbba4838df67b667c415e60dfd8072bc9dff6e
Darien Payton <darien.payton71@gmail.com>
2019-05-09 15:54:50 -0400

    rendered routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Route, NavLink, Link} from 'react-router-dom';
+
+import Signup from './components/Signup';
+import Login from './components/Login';
+import Users from './components/Users';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavLink to='/signup'>Sign Up</NavLink>
+      <NavLink to='/login'>Log In</NavLink>
+
+      <Route path='/signup' component={Signup} />
+      <Route path='/login' component={Login} />
+      <Route path='/users' component={Users} />
     </div>
   );
 }
